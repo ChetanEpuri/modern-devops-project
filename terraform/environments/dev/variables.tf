b87927d0320de1189e87c25276a65a6de7e6e8b2
Chetan <149045417+ChetanEpuri@users.noreply.github.com>
2026-05-31 23:13:08 +0530

    feat: Add dev environment variables

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name"      { type = string }
+variable "environment"       { type = string }
+variable "aws_region"        { type = string }
+variable "gcp_region"        { type = string }
+variable "gcp_project_id"    { type = string }
+variable "aws_instance_type" { type = string }
+variable "gcp_instance_type" { type = string }
